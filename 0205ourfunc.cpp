8a398cc745a53cd5434ea99eeb0120a0ec4ae9cc
Roy <tjubme2012jj@163.com>
2018-04-16 23:03:54 +0800

    Add files via upload

---
+++ 0205ourfunc.cpp
@@ -0,0 +1,24 @@
+//// ourfunc.cpp -- defining your own function
+//#include <iostream>
+//void simon(int);
+//
+//int main()
+//{
+//	using namespace std;
+//	simon(3);
+//	cout << "Pick an integer: ";
+//	int count;
+//	cin >> count;
+//	simon(count);
+//	cout << "Done!" << endl;
+//	cin.get();
+//	cin.get();
+//	return 0;
+//
+//}
+//
+//void simon(int n)
+//{
+//	using namespace std;
+//	cout << "Simon says touch your tors " << n << " times." << endl;
+//}
---
+++ 0206convert.cpp
@@ -0,0 +1,21 @@
+//convert.cpp -- converts stone ti pounds
+#include <iostream>
+int stonetolb(int);
+int main()
+{
+	using namespace std;
+	int stone;
+	cout << "Enter your weight in stone: ";
+	cin >> stone;
+	int pounds = stonetolb(stone);
+	cout << stone << " stone = ";
+	cout << pounds << " pounds." << endl;
+	cin.get();
+	cin.get();
+	return 0;
+}
+
+int stonetolb(int sts)
+{
+	return 14 * sts;
+}
